b71c5007fea49b4d426bb761a1ac0095d894c343
AxelsFirst <alex.gibala@protonmail.com>
2021-08-01 19:53:43 +0200

    Utworzenie klasy grafu i poprawa metryki

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import math
 import numpy as np
 
-wymiar = 2
-
 class punkt:
-    def ustaw_wsp(self, wymiar):
+    def ustaw_wsp(self):
         self.lista_wsp = []
-        for i in range(wymiar):
+        for i in range(self.wymiar):
             self.wsp.append(input('Wypisz {}-tą współrzędną: '.format(i)))
 
     def __init__(self, nazwa, wymiar):
         self.nazwa = nazwa
-        self.ustaw_wsp(wymiar)
+        self.wymiar = wymiar
+        self.ustaw_wsp()
     
     def wypisz_wsp(self, wydrukuj = False):
         if wydrukuj == True:
@@ -23,62 +22,72 @@ class punkt:
         self.lista_wsp[indeks] = wartosc
 
     def __add__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] + other.lista_wsp[i])
-        return wynik
+        suma = []
+        for i in range(self.wymiar):
+            suma.append(self.lista_wsp[i] + other.lista_wsp[i])
+        return suma
 
     def __iadd__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] + other.lista_wsp[i])
-        return wynik
+        suma = []
+        for i in range(self.wymiar):
+            suma.append(self.lista_wsp[i] + other.lista_wsp[i])
+        return suma
 
     def __sub__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] - other.lista_wsp[i])
-        return wynik
+        roznica = []
+        for i in range(self.wymiar):
+            roznica.append(self.lista_wsp[i] - other.lista_wsp[i])
+        return roznica
 
     def __isub__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] - other.lista_wsp[i])
-        return wynik
+        roznica = []
+        for i in range(self.wymiar):
+            roznica.append(self.lista_wsp[i] - other.lista_wsp[i])
+        return roznica
 
     def __mul__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] * other.lista_wsp[i])
-        return wynik
+        iloczyn = []
+        for i in range(self.wymiar):
+            iloczyn.append(self.lista_wsp[i] * other.lista_wsp[i])
+        return iloczyn
 
     def __imul__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] * other.lista_wsp[i])
-        return wynik
+        iloczyn = []
+        for i in range(self.wymiar):
+            iloczyn.append(self.lista_wsp[i] * other.lista_wsp[i])
+        return iloczyn
 
     def __div__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] / other.lista_wsp[i])
-        return wynik
+        iloraz = []
+        for i in range(self.wymiar):
+            iloraz.append(self.lista_wsp[i] / other.lista_wsp[i])
+        return iloraz
 
     def __idiv__(self, other):
-        wynik = []
-        for i in range(wymiar):
-            wynik.append(self.lista_wsp[i] / other.lista_wsp[i])
-        return wynik
+        iloraz = []
+        for i in range(self.wymiar):
+            iloraz.append(self.lista_wsp[i] / other.lista_wsp[i])
+        return iloraz
 
     def norma(self):
         suma = 0
-        for i in wymiar:
-            suma += self.lista_wsp[i]**2
+        for wsp in self.lista_wsp:
+            suma += wsp**2
         return math.sqrt(suma)
 
     def metryka(self, other):
-        return norma(self - other)
+        return self.norma(self - other)
 
 class graf:
-    def __init__(self, lista_punkt):
+    def ustaw_liste_punkt(self):
+        lista_punkt = []
+        for i in range(self.liczba_punkt):
+            nazwa_punkt = input('Podaj nazwę punktu: ')
+            punkt(nazwa_punkt, self.wymiar)
+            lista_punkt.append(nazwa_punkt)
         self.lista_punkt = lista_punkt
+
+    def __init__(self, liczba_punkt, wymiar):
+        self.liczba_punkt = liczba_punkt
+        self.wymiar = wymiar
+        self.ustaw_liste_punkt()
