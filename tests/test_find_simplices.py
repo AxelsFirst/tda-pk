e3c61fd26f3b2480b57d6bbdab0f034f6b1538c9
Brian <brian.esiobu@gmail.com>
2021-10-30 19:41:16 +0200

    Unit testing
    Add test case for find simplices.
    
    Issue: #8

--- tests/test_find_simplices.py
+++ tests/test_find_simplices.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+import random
 
 sys.path.append(os.path.join(os.getcwd(), '.'))
 
@@ -26,6 +27,85 @@ class TestFindSimplices(unittest.TestCase):
         self.assertEqual(len(simplices), 1)
         names = sorted(p.name for p in simplices[0])
         self.assertEqual(names, ['A', 'B', 'C', 'D'])
+
+    def test_find_simplices_all_disconnected(self):
+        names = ['A', 'B', 'C', 'D', 'E', 'F']
+        coords = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]
+
+        self.vcr = VietorisRipsComplex.from_list(
+            names=names,
+            coords=coords,
+            epsilon=1/2
+        )
+
+        self.vcr.create_graph()
+
+        simplices = self.vcr.find_simplices()
+        names = list()
+        for simplex in simplices:
+            names.append(simplex[0].name)
+
+        self.assertEqual(len(simplices), 6)
+        self.assertEqual(sorted(names), ['A', 'B', 'C', 'D', 'E', 'F'])
+
+    def test_find_simplices_2_disconnected_graphs(self):
+        names = ['A', 'B', 'C', 'D', 'E', 'F']
+        coords = [[0, 0], [1, 0], [0, 1],
+                  [10, 10], [11, 10], [11, 11]]
+
+        self.vcr = VietorisRipsComplex.from_list(
+            names=names,
+            coords=coords,
+            epsilon=2
+        )
+
+        self.vcr.create_graph()
+
+        simplices = self.vcr.find_simplices()
+
+        self.assertEqual(len(simplices), 2)
+
+        first = tuple(sorted(p.name for p in simplices[0]))
+        second = tuple(sorted(p.name for p in simplices[1]))
+        graphs = set([first, second])
+        self.assertIn(first, graphs)
+        self.assertIn(second, graphs)
+
+    def test_find_simplices_random_graph_1(self):
+        random.seed(123)
+        names, coords = list(), list()
+        for i in range(25):
+            names.append(str(i))
+            coords.append([random.random() for _ in range(2)])
+
+        self.vcr = VietorisRipsComplex.from_list(
+            names=names,
+            coords=coords,
+            epsilon=1/5
+        )
+
+        self.vcr.create_graph()
+
+        simplices = self.vcr.find_simplices()
+        self.assertEqual(len(simplices), 17)
+
+    def test_find_simplices_random_graph_2(self):
+        random.seed(123)
+        names, coords = list(), list()
+        for i in range(50):
+            names.append(str(i))
+            coords.append([random.random() for _ in range(2)])
+
+        self.vcr = VietorisRipsComplex.from_list(
+            names=names,
+            coords=coords,
+            epsilon=1/2
+        )
+
+        self.vcr.create_graph()
+
+        simplices = self.vcr.find_simplices()
+        self.assertEqual(len(simplices), 58)
 
 
 if __name__ == '__main__':
